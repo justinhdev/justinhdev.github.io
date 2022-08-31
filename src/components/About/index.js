870ca5ea2a1143017ff22338a5843328e76342e7
jhancockdev <jhancockpta@gmail.com>
2022-08-31 16:50:39 -0500

    Adding About

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,22 +1,30 @@
-import './index.scss'
-import AnimatedLetters from '../AnimatedLetters'
+import "./index.scss";
+import AnimatedLetters from "../AnimatedLetters";
+import { useEffect, useState } from "react";
 
 const About = () => {
-    return (
-        <div className='container about-page'>
-            <div className='text-zone'>
-                <h1>
-                    <AnimatedLetters
-                        strArray={['A', 'b', 'o', 'u', 't', ' ', 'M', 'e', '.']}
-                        idx={15}
-                    />
-                </h1>
-                <p>
-                    Placeholder text.
-                </p>
-            </div>
-        </div>
-    )
-}
+  const [letterClass, setLetterClass] = useState("text-animate");
 
-export default About
+  useEffect(() => {
+    setTimeout(() => {
+      return setLetterClass("text-animate-hover");
+    }, 3000);
+  }, []);
+
+  return (
+      <div className="about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={["A", "b", "o", "u", "t", " ", "M", "e", "."]}
+              idx={15}
+            />
+          </h1>
+          <p>Placeholder text.</p>
+        </div>
+      </div>
+  );
+};
+
+export default About;
