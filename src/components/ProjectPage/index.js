0dcb3a9ece162c6197fb086a393fb77272ee17c1
jhancockdev <jhancockpta@gmail.com>
2022-09-27 18:02:04 -0500

    project updates

--- src/components/ProjectPage/index.js
+++ src/components/ProjectPage/index.js
@@ -5,6 +5,9 @@ import AnimatedLetters from "../AnimatedLetters";
 import { useEffect, useState } from "react";
 import Aos from "aos";
 import image1 from "../../assets/images/cyberss.jpg";
+import image2 from "../../assets/images/webpong.png";
+import image3 from "../../assets/images/noteapp.png";
+import image4 from "../../assets/images/githublogo.png";
 import Particles from "../Particle/index2";
 
 function ProjectPage() {
@@ -31,21 +34,23 @@ function ProjectPage() {
           <AnimatedLetters
             letterClass={letterClass}
             strArray={projectArray}
-            idx={1}/>
+            idx={1}
+          />
         </h1>
         <h1>
           <AnimatedLetters
             letterClass={letterClass}
             strArray={iveArray}
-            idx={1}/>
+            idx={1}
+          />
         </h1>
         <h1>
           <AnimatedLetters
             letterClass={letterClass}
             strArray={workingArray}
-            idx={1}/>
+            idx={1}
+          />
         </h1>
-        
       </h1>
 
       <div data-aos="fade-down" className="spacer2 layer3"></div>
@@ -58,13 +63,33 @@ function ProjectPage() {
         >
           <Project
             img={image1}
-            description="A reddit bot coded in python using PRAW API."
+            description="Reddit Bot: Optical Character Recognition"
           />
         </a>
-        <Project img="" description="In development!" />
-        <Project img="" description="In development!" />
-        <Project img="" description="In development!" />
-        <Project img="" description="In development!" />
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://web-2pong.netlify.app/"
+        >
+          <Project
+            img={image2}
+            description="WebSocket-Pong: Online Multiplayer Game"
+          />
+        </a>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://flask-crud-note-app.herokuapp.com/"
+        >
+          <Project img={image3} description="To-Do App: Flask/SQLAlchemy note app" />
+        </a>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/justinhdev"
+        >
+        <Project img={image4} description="Check out my GitHub to see my code!" />
+        </a>
       </div>
       <div className="particle">
         <Particles />
